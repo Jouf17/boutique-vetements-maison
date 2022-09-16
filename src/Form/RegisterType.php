2b248a56779713c5dfbe5c3d5db9d312b479c321
Jouf17 <48128688+Jouf17@users.noreply.github.com>
2022-09-16 16:30:29 +0200

    update register 1

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -29,22 +30,21 @@ class RegisterType extends AbstractType
                 ]
             ])
             ->add('email', EmailType::class, [
-                'label' => 'Votre mot de passe',
+                'label' => 'Votre Email',
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre adresse email'
                 ]
             ])
-            ->add('password', PasswordType::class, [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques.',
                 'label' => 'Votre mot de passe',
-                'attr' => [
-                    'placeholder' => 'Merci de saisir un mot de passe'
-                ]
-            ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Confirmez votre mot de passe',
-                'mapped' => false,
-                'attr' => [
-                    'placeholder' => 'Merci de confirmer votre mot de passe'
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe'
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe'
                 ]
             ])
             ->add('submit', SubmitType::class, [
