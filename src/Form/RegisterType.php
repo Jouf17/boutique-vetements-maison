da9b0768e5f2476b15b07af6fd018450be179706
Jouf17 <48128688+Jouf17@users.noreply.github.com>
2022-09-16 16:40:56 +0200

    update register 4

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -45,10 +45,16 @@ class RegisterType extends AbstractType
                 'label' => 'Votre mot de passe',
                 'required' => true,
                 'first_options' => [
-                    'label' => 'Mot de passe'
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe'
+                    ]
                 ],
                 'second_options' => [
-                    'label' => 'Confirmez votre mot de passe'
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de confirmer votre mot de passe'
+                    ]
                 ]
             ])
             ->add('submit', SubmitType::class, [
