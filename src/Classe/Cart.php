7374b8ce5bed643da0ef9da5cda9762881d3848b
Jouf17 <48128688+Jouf17@users.noreply.github.com>
2022-09-22 20:32:31 +0200

    update cart 2

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,15 +2,19 @@
 
 namespace App\Classe;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
     private $session;
+    private $entityManager;
 
-    public function __construct(SessionInterface $session)
+    public function __construct(SessionInterface $session, EntityManagerInterface $entityManager)
     {
         $this->session = $session;
+        $this->entityManager = $entityManager;
     }
 
     public function add($id) 
@@ -33,5 +37,46 @@ class Cart
     public function remove() 
     {
         return $this->session->remove('cart');
+    }
+
+    public function delete($id) 
+    {
+        $cart = $this->session->get('cart', []);
+
+        unset($cart[$id]);
+
+        $this->session->set('cart', $cart);
+    }
+
+    public function decrease($id) 
+    {
+        $cart = $this->session->get('cart', []);
+
+        if ($cart[$id] > 1) {
+            $cart[$id]--;
+        } else {
+            unset($cart[$id]);
+        }
+
+        $this->session->set('cart', $cart);
+    }
+
+    public function getFull() 
+    {
+        $cartComplete = [];
+        if ($this->get()) {
+            foreach($this->get() as $id => $quantity) {
+                $productObject = $this->entityManager->getRepository(Product::class)->findOneById($id);
+                if (!$productObject) {
+                    $this->delete($id);
+                    continue;
+                }
+                $cartComplete[] = [
+                    'product' => $productObject,
+                    'quantity' => $quantity
+                ];
+            }
+        }
+        return $cartComplete;
     }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,36 +3,19 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
-use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
-{
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
-    
+{   
     /**
      * @Route("/mon-panier", name="cart")
      */
     public function index(Cart $cart): Response
-    {
-        $cartComplete = [];
-        foreach($cart->get() as $id => $quantity) {
-            $cartComplete[] = [
-                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
-                'quantity' => $quantity
-            ];
-        }
-        
+    {       
         return $this->render('cart/index.html.twig', [
-            'cart' => $cartComplete,
+            'cart' => $cart->getFull(),
         ]);
     }
 
@@ -43,9 +26,7 @@ class CartController extends AbstractController
     {
         $cart->add($id);
 
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        return $this->redirectToRoute('cart');
     }
 
     /**
@@ -55,8 +36,26 @@ class CartController extends AbstractController
     {
         $cart->remove();
 
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="delete_to_cart")
+     */
+    public function delete(Cart $cart, $id): Response
+    {
+        $cart->delete($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/cart/decrease/{id}", name="decrease_to_cart")
+     */
+    public function decrease(Cart $cart, $id): Response
+    {
+        $cart->decrease($id);
+
+        return $this->redirectToRoute('cart');
     }
 }
