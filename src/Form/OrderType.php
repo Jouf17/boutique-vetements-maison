2c50a79249f1e07044be1bb3faad0ff40ae2d2af
Jouf17 <48128688+Jouf17@users.noreply.github.com>
2022-10-04 18:45:20 +0200

    order managing add choice carrier

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Adress;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Carrier;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class OrderType extends AbstractType
@@ -20,6 +21,13 @@ class OrderType extends AbstractType
                 'required' => true,
                 'class' => Adress::class,
                 'choices' => $user->getAdresses(),
+                'multiple' => false,
+                'expanded' => true
+            ])
+            ->add('carriers', EntityType::class, [
+                'label' => 'Choisissez votre transporteur',
+                'required' => true,
+                'class' => Carrier::class,
                 'multiple' => false,
                 'expanded' => true
             ])
